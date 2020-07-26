dbd2e693af1588b331d51c324ebe865f21dce691
velmor99 <volgagaz24100@gmail.com>
2020-07-26 22:57:57 +0300

    Updates

---
+++ precache-manifest.1188fcc3708efd1528f37ecdf9c01596.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4d0b38171225d47c37d3a812e7784143",
+    "url": "/goit-react-hw-01-components/index.html"
+  },
+  {
+    "revision": "10f8e35ad1e2c0e82ca8",
+    "url": "/goit-react-hw-01-components/static/css/main.b4df573b.chunk.css"
+  },
+  {
+    "revision": "e5d694f9862f6ad82a3d",
+    "url": "/goit-react-hw-01-components/static/js/2.bd40daaf.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/goit-react-hw-01-components/static/js/2.bd40daaf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "10f8e35ad1e2c0e82ca8",
+    "url": "/goit-react-hw-01-components/static/js/main.af0cc24a.chunk.js"
+  },
+  {
+    "revision": "7c1abe1df921e466788f",
+    "url": "/goit-react-hw-01-components/static/js/runtime-main.e943b22f.js"
+  }
+]);
